bdeae5822c77083d53b115e6e846930704914f73
AnnaMoudra <anna.moudra@gmail.com>
2021-11-16 12:32:40 +0100

    event loading changed to chunks

--- scripts/exporter.py
+++ scripts/exporter.py
@@ -1,7 +1,8 @@
 import json
-from multiprocessing import Pool, Process, Manager
+from multiprocessing import Pool, Process, Manager, Queue
 import os, sys, gc
 from memory_profiler import profile
+import itertools
 
 import geopandas as gpd
 import numpy as np
@@ -24,11 +25,7 @@ class Exporter:
         self.load_network(network_path)
         self.agent_type = agent_type
         self.export_mode = export_mode
-        if(agent_type == "agent"):
-            if(self.export_mode == CARS):
-                self.load_transport(['car'])
-            else: #load all
-                self.load_transport()
+        
 
 
     def count_agents(self):
@@ -56,34 +53,26 @@ class Exporter:
                                 else [-float(a), -float(b)] 
                                 for x,y,a,b in zip(coords_x, coords_y, i_x, i_y)]
         return new_coords
-
-
-    def load_events(self): #unused: memory hog
-        self.events = pd.read_json(OUTPUT+"events/"+self.agent_type+".json")
-        print("Loaded agents", self.agent_type,"#:", self.events.shape[0])
-        display(self.events.info())
-        self.events.sort_values("id", kind="stable", inplace=True)
-        self.events.set_index("id", inplace=True)
-        print("Memory (kB):",self.events.memory_usage(index=True).sum()/1000)
-
-
-    def load_events_chunk(self, chunk_size):
-        reader = pd.io.json.read_json(OUTPUT+"events/"+self.agent_type+".json", lines=True, orient='records', chunksize=chunk_size)
-        return reader
+    
 
     def load_transport(self, vehicle_types = ["car","tram","subway","bus","funicular"]):
         #load all transport event files for later linking (now about 9GB in RAM :3 sorry future me)
-        self.transport = {}
+        transport = {}
 
-        for file in os.listdir(OUTPUT+"events/"):
-            if file.split('.')[0] in vehicle_types:
-                print("Loading:", file)
-                df = pd.read_json(OUTPUT+"events/"+file, lines=True, orient='records')
+        for folder in os.listdir(OUTPUT+"events/"):
+            if folder in vehicle_types:
+                #load from chunks and concat
+                print("Loading:", folder)
+                df = pd.DataFrame()
+                for file in os.listdir(folder):
+                    df.append(pd.read_json(folder+"/"+file, lines=True, orient='records'))
+
                 df.sort_values("id", kind="stable", inplace=True)
                 df.set_index("id", inplace=True)
                 self.transport[str(file.split('.')[0])] = df.copy()
                 del df
         print("Transport loading finished.")
+        return transport
 
     def link_network(self, df):
         #join links and coordinates
@@ -96,44 +85,39 @@ class Exporter:
         df['coords_from'] = self.return_coords(df.coords_x,df.coords_y, df.x_from, df.y_from) 
         return df
 
+    def pick_vehicle_events(self, df, vehicle_type,  v_id):
+        veh_events = pd.DataFrame()
+        if(vehicle_type == 'car'):
+            veh_row = self.transport['car'].loc[int(v_id)]
+        else:
+            veh_row = self.transport[vehicle_type].loc[int(v_id)]
+        vehicle = pd.DataFrame.from_dict(veh_row["events"])
+
+        starts = df.iloc[np.where(df.vehicle_id == v_id)].loc[np.where(df.type == "PersonEntersVehicle")].time.to_list()
+        ends = df.iloc[np.where(df.vehicle_id == v_id)].loc[np.where(df.type == "PersonLeavesVehicle")].time.to_list()
+
+        vehicle["vehicle_id"] = v_id
+        for start,dest in zip(starts,ends):
+            veh_events = veh_events.append(vehicle.iloc[np.where((vehicle["time"] >= start) & (vehicle["time"]<= dest))])
+        return veh_events
+
+
     def append_vehicles(self, df, agent_id, vehicle_ids, verbal=False):
         events = pd.DataFrame()
         for v_id in vehicle_ids:
             if v_id is not None:
-                veh_events = pd.DataFrame()
                 if(verbal):
                     print("Vehicle id:",v_id)
 
-                if(str(v_id).isnumeric()):
+                if str(v_id).isnumeric():
                     if(verbal):
                         print("\tVehicle type:", "car")
-                    car_row = self.transport['car'].loc[int(v_id)]
-                    car = pd.DataFrame.from_dict(car_row["events"])
-
-                    starts = df.iloc[np.where(df.vehicle_id == v_id)].loc[np.where(df.type == "PersonEntersVehicle")].time.to_list()
-                    ends = df.iloc[np.where(df.vehicle_id == v_id)].loc[np.where(df.type == "PersonLeavesVehicle")].time.to_list()
-
-                    car["vehicle_id"] = v_id
-                    for start,dest in zip(starts,ends):
-                        veh_events = veh_events.append(car.iloc[np.where((car["time"] >= start) & (car["time"]<= dest))])
-                
+                    veh_events = self.pick_vehicle_events(df, "car", v_id)
+                    
                 elif  v_id.split('_')[-1] in self.transport.keys():
                     veh_type = v_id.split('_')[-1]
-                
-                    veh_row = self.transport[veh_type].loc[v_id]
-                    vehicle = pd.DataFrame.from_dict(veh_row["events"])
-                    vehicle["vehicle_id"] = v_id
+                    veh_events = self.pick_vehicle_events(df, veh_type, v_id)
 
-                    a = df.iloc[np.where(df.vehicle_id == v_id)] #pick start and end points of vehicle interaction(s)
-                    starts = a.iloc[np.where(a.type == "PersonEntersVehicle")].time.to_list()
-                    ends = a.iloc[np.where(a.type == "PersonLeavesVehicle")].time.to_list()
-                    for start,dest in zip(starts,ends):
-                        if(verbal):
-                            print(start, dest)
-                            display(vehicle.iloc[np.where((vehicle["time"] >= start) & (vehicle["time"]<= dest))])
-                        veh_events = veh_events.append(vehicle.iloc[np.where((vehicle["time"] >= start) & (vehicle["time"]<= dest))])
-
-                #drop events
                 if('type' in veh_events.columns):
                     drop_idx = veh_events[
                                 ((veh_events['type'] == "PersonEntersVehicle") & (veh_events['person_id'] != str(agent_id))) | #
@@ -146,15 +130,10 @@ class Exporter:
                                 ].index      
                     veh_events.drop(drop_idx, inplace=True)
 
-                
-
                 events = events.append(veh_events)
                 del veh_events
-                del vehicle
-                del veh_row
 
         df = df.append(events, ignore_index=True)
-        #df = self.link_network(df)
         df = df.sort_values(["time"], kind="stable") #, "type"
         del events
         gc.collect()
@@ -168,7 +147,7 @@ class Exporter:
         
         return False
 
-    @profile
+    
     def link_transport(self,df, agent_id):
         vehicle_ids = [ x for x in list(df.vehicle_id.unique())] #veh ids
         #print("Vehicle ids:", vehicle_ids)
@@ -187,6 +166,7 @@ class Exporter:
         df["person_id"] = agent_id
         return df
 
+    #@profile
     def prepare_agent(self, row, agent_id, verbal=False):
         #prep agent
         v = pd.DataFrame.from_dict(row["events"])
@@ -244,12 +224,12 @@ class Exporter:
             if a != None and (output_type == 'shp'):
                 a.prepare_geotrips()
                 output = output.append(a.geotrips.copy())
-            #else:
-            #    print("implement (geo)json support")
-            #    return
+            elif output_type == 'json':
+                print("implement (geo)json support")
+                return
             del a
 
-        #save chunks
+        #save chunk
         if(output_type == 'shp' and not output.empty):
             #reset trip_index
             output.reset_index(inplace=True)
@@ -267,40 +247,63 @@ class Exporter:
         gc.collect()
 
 
-    def chunk_task(self, chunk_i, ids, path_prefix, format):
-        #path =  path_prefix+self.agent_type+'_sec_'+str(chunk_i)+'.'+format
+    def chunk_task(self, chunk_path, path_prefix, format):
+        print("Loading:",chunk_path)
+        chunk = pd.read_json(chunk_path, lines=True, orient='records')
+        chunk_i = int(chunk_path.split('/')[-1].split('.')[0])
         path =  path_prefix+self.agent_type+'_sec_'+str(chunk_i)+'.'+format
-        self.extract_chunk(ids, output_type = format, path=path, verbal=False)
+        print("Agents in chunk", chunk_i, ":", chunk.shape[0])
+        self.extract_chunk(chunk, output_type = format, path=path, verbal=False)
         print("Chunk saved to:",path)
 
-        
-    def export_agents(self, chunk_size = CHUNK_SIZE, format = OUTPUT_FORMAT, parallel=PARALLEL):
-        path_prefix = OUTPUT+'matsim_vehicles_'+str(format)+'/chunks'+str(chunk_size)+'/'   
-        event_reader = self.load_events_chunk(chunk_size) 
+    def chunk_task(self, args):
+        chunk_path, path_prefix, format = args
+        print("Loading:",chunk_path)
+        chunk = pd.read_json(chunk_path, lines=True, orient='records')
+        chunk_i = int(chunk_path.split('/')[-1].split('.')[0])
+        path =  path_prefix+self.agent_type+'_sec_'+str(chunk_i)+'.'+format
+        self.extract_chunk(chunk, output_type = format, path=path, verbal=False)
+        print("Chunk saved to:",path)
+
+
+    def parallel_run(self, files, proc, path_prefix, format):
+        if(self.agent_type == "agent"):
+            if(self.export_mode == CARS):
+                self.transport = self.load_transport(['car'])
+            else: #load all
+                self.transport = self.load_transport()
+
+        args = list()
+        for f in files:
+            args.append([f, path_prefix, format])
+
+        with Pool(proc) as pool:
+            pool.map(self.chunk_task, args)
+
+        pool.join()
+        pool.close()
+
+    def export_agents(self, chunk_size = CHUNK_SIZE, format = OUTPUT_FORMAT, parallel=PARALLEL, proc=4):
+        path_prefix = OUTPUT+'matsim_agents_'+str(format)+'/chunks'+str(chunk_size)+'/'   
+        if not os.path.exists(path_prefix):
+                os.makedirs(path_prefix)
+        #event_reader = self.load_events_chunk(chunk_size) 
+        dirc = OUTPUT+"events/"+self.agent_type
+        files = [ dirc+"/"+f for f in os.listdir(dirc)]
         chunk_i = 0
         if(parallel):
-            p_list = []
-            #manager = Manager()
-            #ns = manager.Namespace()
-            #if(self.agent_type == "agent"):
-                
-            for chunk in event_reader:
-                # Instantiates the thread
-                print("Agents in chunk", chunk_i, ":", chunk.shape[0])
-                p = Process(target=self.chunk_task, args=(chunk_i, chunk, path_prefix, format))
-                chunk_i+=1
-                p.start()
-                p_list.append(p)
-
-            for t in p_list:
-                t.join()
+            self.parallel_run(files, proc, path_prefix, format)
 
         else:
-            for chunk in event_reader:
-                print("Agents in chunk", chunk_i, ":", chunk.shape[0])
-                self.chunk_task(chunk_i,chunk,path_prefix,format)
-                chunk_i +=1
+            if(self.agent_type == "agent"):
+                if(self.export_mode == CARS):
+                    self.transport = self.load_transport(['car'])
+                else: #load all
+                    self.transport = self.load_transport()
 
+            for file in files:
+                self.chunk_task(file,path_prefix,format)
+                chunk_i +=1
         return
         
 
